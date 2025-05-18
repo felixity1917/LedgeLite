4615af69a360097ebeeb6dcb86bc3638931dbef9
felixity1917 <77635620+felixity1917@users.noreply.github.com>
2025-05-18 20:05:54 +0530

    #Test

--- src/main.cxx
+++ src/main.cxx
@@ -2,7 +2,7 @@
 #include <string>
 #include <cmath>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char **argv[]) {
 	std::cout << "Hello World! Welcome to LedgeLite." << std::endl;
 	return 0;
 }
