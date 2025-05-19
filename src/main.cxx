799b801f87395e482b7b6fa7ac413d9aac8a5310
felixity1917 <77635620+felixity1917@users.noreply.github.com>
2025-05-19 17:02:38 +0530

    Add: Main Loop

--- src/main.cxx
+++ src/main.cxx
@@ -1,8 +1,37 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+using namespace std;
+
+int startAmount = 0;
 
 int main(int argc, char **argv[]) {
-	std::cout << "Hello World! Welcome to LedgeLite." << std::endl;
+	cout << "Welcome to LedgeLite." << endl;
+	bool termFlag = false;
+	while (!termFlag) {
+		cout << "Enter Choice" << endl;
+		cout << "1 - Show Previous Transactions" << endl;
+		cout << "2 - Add new entry" << endl;
+		cout << "3 - Exit" << endl;
+		int choice;
+		cin >> choice;
+		switch(choice) {
+			case 1: {
+				Transaction::showPrevious();
+				break;
+			}
+			case 2: {
+				Transaction::addEntry();
+				break;
+			}
+			case 3: {
+				cout << "Thanks for using LedgeLite." << endl;
+				return 0;
+			}
+			default: {
+				cout << "Invalid Choice!" << endl;
+			}
+		}
+	}
 	return 0;
 }
